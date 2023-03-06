a54ffc12abb2b2350bc3690cb631302aa3a03f4b
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-06 17:39:15 -0300

    desenvolvimento do código do enpoint post de login; há algo errado com a validação dos dados no DB

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,29 +1,28 @@
 const { loginService } = require('../services');
+const { createToken } = require('../middlewares/createToken');
 
 const inspectBodyInfo = (email, password) => email && password; // exemplo da aula do zambs, lecture/backend 6.4
 
 module.exports = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email);
-    console.log(password);
 
     if (!inspectBodyInfo(email, password)) {
         return res.status(400).json({ message: 'Some required fields are missing' });
     }
 
     // Será validado que não é possível fazer login com um usuário que não existe
-    // Precisamos buscar no DB pelo email ou senha. Então essa função de validação é um service
-
-    const isValid = await loginService.validateUser(email, password);
-    if (!isValid) {
+    const user = await loginService.validateUser(email, password);
+    if (!user) {
         return res.status(400).json({ message: 'Invalid fields' });
     }
 
     // se as infos do login estiverem ok, criar um token (fazer uma func auxiliar)
+    const { password: _, ...userWithoutPassword } = user.dataValues;
+    const token = createToken(userWithoutPassword);
 
     // Será validado que é possível fazer login com sucesso
-    res.status(200).json({ token: 'inserirTokenAqui' });
+    res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({
         message: 'Something is off',
