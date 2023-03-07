88a18fe43ab390cdc21e11c54a48a27ee2a3dcfc
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-07 17:09:43 -0300

    ajustes na validação do token - mudança da mensagem de erro do catch e mudança na função de listar usuários, de forma a não exibir a senha

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -5,7 +5,6 @@ require('dotenv/config');
 const secret = process.env.JWT_SECRET || 'MySecretWord';
 
 const validateToken = async (req, res, next) => {
-  console.log(req.header); // retorna [Function: header]
   const token = req.header('Authorization');
   console.log(token); // retorna undefined
   
@@ -13,21 +12,16 @@ const validateToken = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, secret);
-    console.log(decoded);
     const user = await userService.getUserById(decoded.data.id);
     req.user = user;
-    
     next();
     } catch (err) {
-        res.status(500).json({
-            message: 'Something is off',
-            error: err.message,
-          });
-      }
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
 };
 
 module.exports = {
     validateToken,
 };
 
-// resolução inspirada na aula do zambs (lecture/backend 6.4)
+// resolução inspirada no tutorial do Course
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,12 @@ const { User } = require('../models');
 const createUser = ({ displayName, email, password, image }) =>
   User.create({ displayName, email, password, image });
 
-const getAllUsers = () => User.findAll();
+const getAllUsers = async () => {
+  const allUsers = await User.findAll({
+    attributes: ['id', 'displayName', 'email', 'image'],
+  });
+  return allUsers;
+};
 
 const getUserById = (id) => User.findByPk(id);
 
